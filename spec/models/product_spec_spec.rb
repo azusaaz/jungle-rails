1e808313a513cc96948de74373250b55f687d469
Az <shimazaki@aaa.email.ne.jp>
2018-12-10 20:53:15 +0000

    changed test code of product spec

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Product, type: :model do
     
     before do
       @category = Category.create(:name => "Kitchen")
-      @product = Product.create(:name => "LHL Cup", :price => nil, :quantity => 20, :category => @category)
     end
 
     # if you want to check all in at a time
@@ -18,33 +17,47 @@ RSpec.describe Product, type: :model do
     describe "name" do
       it 'should be exist' do
         # puts @product.inspect
+        @product = Product.create(:name => "LHL Cup", :price => 90, :quantity => 20, :category => @category)
+
         expect(@product.name).to be_present
+        
       end
+
+      # it 'should need a valid name' do
+      #   @product = Product.create(:name => "", :price => 90, :quantity => 20, :category => @category)
+
+      #   puts @product.errors.full_messages
+      #   expect(@product.valid?).to be true
+      # end
 
     end
 
     describe "price" do
       it 'should be exist' do
+        @product = Product.create(:name => "LHL Cup", :price => 90, :quantity => 20, :category => @category)
         expect(@product.price).to be_present
       end
     end
 
     describe "quantity" do
       it 'should be exist' do
+        @product = Product.create(:name => "LHL Cup", :price => 90, :quantity => 20, :category => @category)
         expect(@product.quantity).to be_present
       end
     end
 
     describe "category" do
       it 'should be exist' do
+        @product = Product.create(:name => "LHL Cup", :price => 90, :quantity => 20, :category => @category)
         expect(@product.category).to be_present
       end
     end
 
     describe "error message" do
-      it 'should be exist' do
-        puts @product.errors.full_messages
-        expect(@product.errors.full_messages).to be_present
+      it 'should not be exist' do
+        @product = Product.create(:name => "LHL Cup", :price => 90, :quantity => 20, :category => @category)
+         puts @product.errors.full_messages
+        expect(@product.errors.full_messages).to be false
       end
     end
     
