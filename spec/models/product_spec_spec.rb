60d0e16133b9245293420cbce8eaab7bf7cf2dd1
Az <shimazaki@aaa.email.ne.jp>
2018-12-11 16:38:46 +0000

    fix test product test case except error message block

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Product, type: :model do
     
     before do
       @category = Category.create(:name => "Kitchen")
-      @product = Product.create(:name => "LHL Cup", :price => nil, :quantity => 20, :category => @category)
+
     end
 
     # if you want to check all in at a time
@@ -18,30 +18,35 @@ RSpec.describe Product, type: :model do
     describe "name" do
       it 'should be exist' do
         # puts @product.inspect
-        expect(@product.name).to be_present
+        @product = Product.create(:name => "", :price => 22, :quantity => 20, :category => @category)
+        expect(@product.name).to_not be_present
       end
 
     end
 
     describe "price" do
       it 'should be exist' do
-        expect(@product.price).to be_present
+        @product = Product.create(:name => "LHL Cup", :price => nil, :quantity => 20, :category => @category)
+        expect(@product.price).to_not be_present
       end
     end
 
     describe "quantity" do
       it 'should be exist' do
-        expect(@product.quantity).to be_present
+        @product = Product.create(:name => "LHL Cup", :price => 22, :quantity => nil, :category => @category)
+        expect(@product.quantity).to_not be_present
       end
     end
 
     describe "category" do
       it 'should be exist' do
-        expect(@product.category).to be_present
+        @product = Product.create(:name => "LHL Cup", :price => 22, :quantity => 20, :category => nil)
+        expect(@product.category).to_not be_present
       end
     end
 
     describe "error message" do
+      @product = Product.create(:name => "LHL Cup", :price => nil, :quantity => 20, :category => @category)
       it 'should be exist' do
         puts @product.errors.full_messages
         expect(@product.errors.full_messages).to be_present
