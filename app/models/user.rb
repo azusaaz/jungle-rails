2dba5c5d7ecc38b35681b7dab37b8b669dd787bf
Az <shimazaki@aaa.email.ne.jp>
2018-12-28 16:48:58 +0000

    error occured

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ActiveRecord::Base
   
+  #  has_secure_password
+
+  validates :password, :confirmation => true
+  validates :password_confirmation, :presence => true
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 3}
   
   def authenticate_with_credentials(email, password)
-    if @user = User.find_by('lower(email) = ?', email.strip().downcase).try(:authenticate, password) 
-      return @user
+    # if @user = User.find_by('lower(email) = ?', email.strip().downcase).try(:authenticate, password) 
+    #   return @user
+
+      if @user = User.find_by(email: email).try(:authenticate, password) 
+        return @user
     else
       nil
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181207181240) do
+ActiveRecord::Schema.define(version: 20181211201547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,9 +69,14 @@ ActiveRecord::Schema.define(version: 20181207181240) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "line_items", "orders"
