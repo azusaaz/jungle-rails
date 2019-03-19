a32827527a522dffc519faab2773e6a90103a9c3
Az <shimazaki@aaa.email.ne.jp>
2019-03-19 12:28:45 +0000

    test

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -3,10 +3,8 @@
 class ProductImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  #### include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
-  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
   storage :file
